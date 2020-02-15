923455bc930a138118dcb3fc20f074757231442e
Ozgur Sahin <ozgursahin555@gmail.com>
2020-02-15 18:37:48 +0300

    direct download dropbox

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 
-export_file_url = 'https://www.dropbox.com/s/i17egiz75nlkark/export.pkl'
+export_file_url = 'https://www.dropbox.com/s/i17egiz75nlkark/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
